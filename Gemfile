f7040d2222803f795f43427e0b39ea1c1cbfe2d4
Rob Richardson <robrich@robrich.org>
2020-08-17 13:18:29 -0700

    Getting started with MemSQL and Ruby

---
+++ Gemfile
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+ruby '2.6.3'
+
+gem 'mysql2'
+
+group :development do
+end
---
+++ app.rb
@@ -0,0 +1,60 @@
+require 'mysql2'
+
+def main
+  # TODO: pull from config
+  client = Mysql2::Client.new(:host => "127.0.0.1", :username => "root", :password => "", :database => "acme")
+
+  id = create(client, 'Inserted row')
+  puts "Inserted row #{id}"
+
+  row = read_one(client, id);
+  if (row)
+    puts "Read row: #{row}"
+  else
+    puts "Read row: missing"
+  end
+
+  update(client, id, "Updated row")
+  puts "Updated row id #{id}"
+  
+  rows = read_all(client)
+  puts "Read all rows:"
+  rows.each do |row|
+    puts "  #{row}"
+  end
+
+  delete(client, id)
+  puts "Deleted row id #{id}"
+
+end
+
+def create(client, content)
+  stmt = client.prepare("INSERT INTO messages (content) VALUES (?)")
+  stmt.execute(content)
+  client.last_id
+end
+
+def read_one(client, id)
+  stmt = client.prepare("SELECT id, content, createdate FROM messages WHERE id = ?")
+  result = stmt.execute(id)
+  result.first
+end
+
+def read_all(client)
+  results = client.query("SELECT * FROM messages ORDER BY id")
+  results
+end
+
+def update(client, id, content)
+  stmt = client.prepare("UPDATE messages SET content = ? WHERE id = ?")
+  stmt.execute(content, id)
+end
+
+def delete(client, id)
+  stmt = client.prepare("DELETE FROM messages WHERE id = ?")
+  stmt.execute(id)
+end
+
+if __FILE__ == $0
+  main
+end
